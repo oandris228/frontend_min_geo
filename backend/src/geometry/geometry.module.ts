a050a8062ffe73a3aabd2a19d2c108cbe0962d8b
MBotond21 <m.botond07@gmail.com>
2024-11-25 10:11:48 +0100

    prisma service

--- backend/src/geometry/geometry.module.ts
+++ backend/src/geometry/geometry.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GeometryService } from './geometry.service';
 import { GeometryController } from './geometry.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [GeometryController],
-  providers: [GeometryService],
+  providers: [GeometryService, PrismaService],
 })
 export class GeometryModule {}
