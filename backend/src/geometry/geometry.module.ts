deb752455f865ab9d3b060bdf15d455f25bab798
P.Boti <botondpasztor@gmail.com>
2024-11-26 23:12:39 +0100

    szerver javítások + listázás

--- backend/src/geometry/geometry.module.ts
+++ backend/src/geometry/geometry.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GeometryService } from './geometry.service';
 import { GeometryController } from './geometry.controller';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [GeometryController],
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
 
+import Listazas from './components/Listazas.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/listazas",
+    element: <Listazas />
+  },
+
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
